47e584c52e1c33058b435e328badd872093a4dc1
Niklas Heim <niklas.heim@pasqal.com>
2023-04-12 16:15:06 +0200

    update bench script

--- examples/bench.py
+++ examples/bench.py
@@ -1,46 +1,32 @@
 import time
 
 import torch
-from pyqtorch.modules import RX, X, CNOT, QuantumCircuit, uniform_state
+from pyqtorch.modules import RX, X, CNOT, QuantumCircuit, uniform_state, zero_state
 from pyqtorch.core.batched_operation import batchedRX
 
-# print(OPERATIONS_DICT["I"])
-# g = OPERATIONS_DICT["I"].to(device="mps", dtype=torch.cfloat)
-# print(g + g)
+def timeit(f, *args, niters=100):
+    t = 0
+    for _ in range(niters):
+        t0 = time.time()
+        f(*args)
+        t1 = time.time()
+        t += t1-t0
+    return t / niters
 
 dtype = torch.cdouble
-device = "cpu"
+device = "cuda"
 batch_size = 1_000
-state = uniform_state(2, batch_size=batch_size, device=device, dtype=dtype)
+qubits = [0]
+n_qubits = 10
+
+state = zero_state(n_qubits, batch_size=batch_size, device=device, dtype=dtype)
 phi = torch.rand(batch_size, device=device, dtype=dtype)
 thetas = {"phi": phi}
 
-qubits = [0]
-n_qubits = 2
-iters = 1_000
-
-t1 = time.time()
-for _ in range(iters):
-    batchedRX(phi, state, qubits, n_qubits)
-print(f"{time.time()-t1}")
+func_time = timeit(batchedRX, phi, state, qubits, n_qubits)
 
 gate = RX(qubits, n_qubits, "phi").to(device=device, dtype=dtype)
-# gate = torch.compile(gate)
-t1 = time.time()
-for _ in range(iters):
-    gate(thetas, state)
-print(f"{time.time()-t1}")
-print(state)
+mod_time = timeit(gate, thetas, state)
 
-ops = [X([0], 2), X([1], 2), RX([1], 2, "phi"), CNOT([0, 1], 2)]
-ops = [RX([0], 2, "phi")]
-circ = QuantumCircuit(2, ops).to(device=device, dtype=dtype)
-
-circ(thetas, state)
-t1 = time.time()
-for _ in range(iters):
-    circ(thetas, state)
-print(f"{time.time()-t1}")
-print(state)
-
-print()
+print(f"Functional pyq: {func_time}")
+print(f"Module pyq:     {mod_time}")
