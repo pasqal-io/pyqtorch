faa9ad3f32b69dbacdd8ab032941477a2fa3a34c
seitzdom <dominik.seitz@pasqal.com>
2023-04-25 15:56:21 +0200

    Added grad test for circuit

--- pyqtorch/modules/hamevo.py
+++ pyqtorch/modules/hamevo.py
@@ -1,26 +1,11 @@
-# Copyright 2022 PyQ Development Team
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from __future__ import annotations
 
+import torch
 from functools import lru_cache
 from typing import Any, Optional, Tuple
 
-import torch
-from numpy.typing import ArrayLike
-
 from pyqtorch.converters.store_ops import ops_cache, store_operation
-from pyqtorch.core.utils import OPERATIONS_DICT, _apply_gate
+from pyqtorch.core.utils import _apply_gate
 
 
 def hamiltonian_evolution(
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -30,15 +30,30 @@ def test_gates(gate: str) -> None:
 
 
 def test_circuit() -> None:
+    n_qubits = 2
     device = "cpu"
     dtype = torch.cdouble
     batch_size = 5
 
-    ops = [pyq.X([0], 2), pyq.X([1], 2), pyq.RX([1], 2, "phi"), pyq.CNOT([0, 1], 2)]
-    circ = pyq.QuantumCircuit(2, ops).to(device=device, dtype=dtype)
+    ops = [
+        pyq.X([0], n_qubits),
+        pyq.X([1], n_qubits),
+        pyq.RX([1], n_qubits, "phi"),
+        pyq.CNOT([0, 1], n_qubits),
+    ]
+    circ = pyq.QuantumCircuit(n_qubits, ops).to(device=device, dtype=dtype)
 
     state = circ.init_state(batch_size)
-    phi = torch.rand(batch_size, device=device, dtype=dtype)
+    phi = torch.rand(batch_size, device=device, dtype=dtype, requires_grad=True)
     thetas = {"phi": phi}
 
     assert circ(thetas, state).size() == (2, 2, 5)
+
+    state = pyq.zero_state(n_qubits, batch_size=batch_size, device=device, dtype=dtype)
+
+    res = circ.forward(thetas, state)
+    assert not torch.all(torch.isnan(res))
+
+    # g = torch.autograd.grad(circ, thetas)
+    dres_theta = torch.autograd.grad(res, phi, torch.ones_like(res), create_graph=True)[0]
+    assert not torch.all(torch.isnan(dres_theta))
