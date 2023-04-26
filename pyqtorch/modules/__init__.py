3aaad24681cbbf073baab560789b70ab53232418
seitzdom <dominik.seitz@pasqal.com>
2023-04-26 09:37:42 +0200

    Added hamevo module

--- pyqtorch/modules/__init__.py
+++ pyqtorch/modules/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
 from pyqtorch.modules.circuit import QuantumCircuit, uniform_state, zero_state
+from pyqtorch.modules.hamevo import HamEvo
 from pyqtorch.modules.parametric import CPHASE, CRX, CRY, CRZ, RX, RY, RZ
 from pyqtorch.modules.primitive import CNOT, SWAP, H, I, S, T, X, Y, Z
