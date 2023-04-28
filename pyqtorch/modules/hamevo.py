2284807c972d07a98e6b1f7b7ab98f18adf6cdd9
seitzdom <dominik.seitz@pasqal.com>
2023-04-28 18:24:12 +0200

    Functional hamevo for eig

--- pyqtorch/modules/hamevo.py
+++ pyqtorch/modules/hamevo.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from typing import Any
+from functools import lru_cache
+from typing import Any, Optional, Tuple
 
 import torch
 
 from pyqtorch.core.utils import _apply_batch_gate
+
+BATCH_DIM = 2
 
 
 class HamEvo(torch.nn.Module):
@@ -12,6 +15,8 @@ class HamEvo(torch.nn.Module):
         self, H: torch.Tensor, t: torch.Tensor, qubits: Any, n_qubits: int, n_steps: int = 100
     ):
         super().__init__()
+        self.H: torch.Tensor
+        self.t: torch.Tensor
         self.qubits = qubits
         self.n_qubits = n_qubits
         self.n_steps = n_steps
@@ -49,159 +54,79 @@ class HamEvo(torch.nn.Module):
         return self.H.device
 
 
-# def hamiltonian_evolution(
-#     H: torch.Tensor,
-#     state: torch.Tensor,
-#     t: torch.Tensor,
-#     qubits: Any,
-#     N_qubits: int,
-#     n_steps: int = 100,
-# ) -> torch.Tensor:
-#     """A function to perform time-evolution according to the generator `H` acting on a
-#     `N_qubits`-sized input `state`, for a duration `t`. See also tutorials for more information
-#     on how to use this gate.
+@lru_cache(maxsize=256)
+def diagonalize(H: torch.Tensor) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
+    """
+    Diagonalizes an Hermitian Hamiltonian, returning eigenvalues and eigenvectors.
+    First checks if it's already diagonal, and second checks if H is real.
+    """
 
-#     Args:
-#         H (torch.Tensor): the dense matrix representing the Hamiltonian,
-#             provided as a `Tensor` object with shape
-#             `(N_0,N_1,...N_(N**2),batch_size)`, i.e. the matrix is reshaped into
-#             the list of its rows
-#         state (torch.Tensor): the input quantum state, of shape `(N_0, N_1,..., N_N, batch_size)`
-#         t (torch.Tensor): the evolution time, real for default unitary evolution
-#         qubits (Any): The qubits support where the H evolution is applied
-#         N_qubits (int): The number of qubits
-#         n_steps (int, optional): The number of steps to divide the time interval
-#             in. Defaults to 100.
+    def is_diag(H: torch.Tensor) -> bool:
+        return len(torch.abs(torch.triu(H, diagonal=1)).to_sparse().coalesce().values()) == 0
 
-#     Returns:
-#         torch.Tensor: replaces state with the evolved state according to the
-#             instructions above (save a copy of `state` if you need further
-#             processing on it)
-#     """
+    def is_real(H: torch.Tensor) -> bool:
+        return len(torch.imag(H).to_sparse().coalesce().values()) == 0
 
-#     if ops_cache.enabled:
-#         store_operation("hevo", qubits, param=t)
+    if is_diag(H):
+        # Skips diagonalization
+        eig_values = torch.diagonal(H)
+        eig_vectors = None
+    else:
+        if is_real(H):
+            eig_values, eig_vectors = torch.linalg.eigh(H.real)
+            eig_values = eig_values.to(torch.cdouble)
+            eig_vectors = eig_vectors.to(torch.cdouble)
+        else:
+            eig_values, eig_vectors = torch.linalg.eigh(H)
 
-#     # batch_size = len(t)
-#     # #permutation = [N_qubits - q - 1 for q in qubits]
-#     # permutation = [N_qubits - q - 1 for q in range(N_qubits) if q not in qubits]
-#     # permutation += [N_qubits - q - 1 for q in qubits]
-#     # permutation.append(N_qubits)
-#     # inverse_permutation = list(np.argsort(permutation))
-
-#     # new_dim = [2] * (N_qubits - len(qubits)) + [batch_size] + [2**len(qubits)]
-#     # state = state.permute(*permutation).reshape((2**len(qubits), -1))
-
-#     h = t.reshape((1, -1)) / n_steps
-#     for _ in range(N_qubits - 1):
-#         h = h.unsqueeze(0)
-
-#     h = h.expand_as(state)
-
-#     # h = h.expand(2**len(qubits), -1, 2**(N_qubits - len(qubits))).reshape((2**len(qubits), -1))
-#     for _ in range(n_steps):
-#         k1 = -1j * _apply_gate(state, H, qubits, N_qubits)
-#         k2 = -1j * _apply_gate(state + h / 2 * k1, H, qubits, N_qubits)
-#         k3 = -1j * _apply_gate(state + h / 2 * k2, H, qubits, N_qubits)
-#         k4 = -1j * _apply_gate(state + h * k3, H, qubits, N_qubits)
-#         # print(state.shape)
-#         # k1 = -1j * torch.matmul(H, state)
-#         # k2 = -1j * torch.matmul(H, state + h/2 * k1)
-#         # k3 = -1j * torch.matmul(H, state + h/2 * k2)
-#         # k4 = -1j * torch.matmul(H, state + h * k3)
-
-#         state += h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-
-#     return state  # .reshape([2]*N_qubits + [batch_size]).permute(*inverse_permutation)
+    return eig_values, eig_vectors
 
 
-# @lru_cache(maxsize=256)
-# def diagonalize(H: torch.Tensor) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
-#     """
-#     Diagonalizes an Hermitian Hamiltonian, returning eigenvalues and eigenvectors.
-#     First checks if it's already diagonal, and second checks if H is real.
-#     """
+class HamEvoEig(HamEvo):
+    def __init__(
+        self, H: torch.Tensor, t: torch.Tensor, qubits: Any, n_qubits: int, n_steps: int = 100
+    ):
+        super().__init__(H, t, qubits, n_qubits, n_steps)
+        if len(self.H.size()) < 3:
+            self.H = self.H.unsqueeze(2)
+        batch_size_h = self.H.size()[BATCH_DIM]
+        self.l_vec = []
+        self.l_val = []
+        for i in range(batch_size_h):
+            eig_values, eig_vectors = diagonalize(self.H[..., [i]])
+            self.l_vec.append(eig_vectors)
+            self.l_val.append(eig_values)
 
-#     def is_diag(H: torch.Tensor) -> bool:
-#         return len(torch.abs(torch.triu(H, diagonal=1)).to_sparse().coalesce().values()) == 0
+    def apply(self, state: torch.Tensor) -> torch.Tensor:
 
-#     def is_real(H: torch.Tensor) -> bool:
-#         return len(torch.imag(H).to_sparse().coalesce().values()) == 0
+        batch_size_t = len(self.t)
+        batch_size_h = self.H.size()[BATCH_DIM]
+        t_evo = torch.zeros(batch_size_h).to(torch.cdouble)
+        evol_operator = torch.zeros(self.H.size()).to(torch.cdouble)
 
-#     if is_diag(H):
-#         # Skips diagonalization
-#         eig_values = torch.diagonal(H)
-#         eig_vectors = None
-#     else:
-#         if is_real(H):
-#             eig_values, eig_vectors = torch.linalg.eigh(H.real)
-#             eig_values = eig_values.to(torch.cdouble)
-#             eig_vectors = eig_vectors.to(torch.cdouble)
-#         else:
-#             eig_values, eig_vectors = torch.linalg.eigh(H)
+        if batch_size_t >= batch_size_h:
+            t_evo = self.t[:batch_size_h]
+        else:
+            if batch_size_t == 1:
+                t_evo[:] = self.t[0]
+            else:
+                t_evo[:batch_size_t] = self.t
 
-#     return eig_values, eig_vectors
+        for i in range(batch_size_h):
+            eig_values, eig_vectors = self.l_val[i], self.l_vec[i]
+            eig_values = eig_values.flatten(0)
 
+            if eig_vectors is None:
+                # Compute e^(-i H t)
+                evol_operator[..., i] = torch.diag(torch.exp(-1j * eig_values * t_evo[i]))
 
-# def hamiltonian_evolution_eig(
-#     H: torch.Tensor,
-#     state: torch.Tensor,
-#     t: torch.Tensor,
-#     qubits: Any,
-#     N_qubits: int,
-# ) -> torch.Tensor:
-#     """A function to perform time-evolution according to the generator `H` acting on a
-#     `N_qubits`-sized input `state`, for a duration `t`. See also tutorials for more information
-#     on how to use this gate. Uses exact diagonalization.
+            else:
+                # Compute e^(-i D t)
+                eig_exp = torch.diag(torch.exp(-1j * eig_values * t_evo[i]))
+                # e^(-i H t) = V.e^(-i D t).V^\dagger
+                evol_operator[..., i] = torch.matmul(
+                    torch.matmul(eig_vectors, eig_exp),
+                    torch.conj(eig_vectors.transpose(0, 1)),
+                )
 
-#     Args:
-#         H (torch.Tensor): the dense matrix representing the Hamiltonian,
-#             provided as a `Tensor` object with shape
-#             `(N_0,N_1,...N_(N**2),batch_size)`, i.e. the matrix is reshaped into
-#             the list of its rows
-#         state (torch.Tensor): the input quantum state, of shape `(N_0, N_1,..., N_N, batch_size)`
-#         t (torch.Tensor): the evolution time, real for default unitary evolution
-#         qubits (Any): The qubits support where the H evolution is applied
-#         N_qubits (int): The number of qubits
-
-#     Returns:
-#         torch.Tensor: replaces state with the evolved state according to the
-#             instructions above (save a copy of `state` if you need further processing on it)
-#     """
-
-#     batch_size_s = state.size()[-1]
-#     batch_size_t = len(t)
-
-#     t_evo = torch.zeros(batch_size_s).to(torch.cdouble)
-
-#     if batch_size_t >= batch_size_s:
-#         t_evo = t[:batch_size_s]
-#     else:
-#         if batch_size_t == 1:
-#             t_evo[:] = t[0]
-#         else:
-#             t_evo[:batch_size_t] = t
-
-#     if ops_cache.enabled:
-#         store_operation("hevo", qubits, param=t)
-
-#     eig_values, eig_vectors = diagonalize(H)
-
-#     if eig_vectors is None:
-#         for i, t_val in enumerate(t_evo):
-#             # Compute e^(-i H t)
-#             evol_operator = torch.diag(torch.exp(-1j * eig_values * t_val))
-#             state[..., [i]] = _apply_gate(state[..., [i]], evol_operator, qubits, N_qubits)
-
-#     else:
-#         for i, t_val in enumerate(t_evo):
-#             # Compute e^(-i D t)
-#             eig_exp = torch.diag(torch.exp(-1j * eig_values * t_val))
-#             # e^(-i H t) = V.e^(-i D t).V^\dagger
-#             evol_operator = torch.matmul(
-#                 torch.matmul(eig_vectors, eig_exp),
-#                 torch.conj(eig_vectors.transpose(0, 1)),
-#             )
-#             state[..., [i]] = _apply_gate(state[..., [i]], evol_operator, qubits, N_qubits)
-
-#     return state
+        return _apply_batch_gate(state, evol_operator, self.qubits, self.n_qubits, batch_size_h)
--- tests/test_module_hamevo.py
+++ tests/test_module_hamevo.py
@@ -5,6 +5,7 @@ import random
 from math import isclose
 
 import numpy as np
+import pytest
 import torch
 
 import pyqtorch.modules as pyq
@@ -18,31 +19,59 @@ torch.use_deterministic_algorithms(True)
 pi = torch.tensor(torch.pi, dtype=torch.cdouble)
 
 
-def test_hamevo_module_single() -> None:
-    n_qubits = 4
+def overlap(state1: torch.Tensor, state2: torch.Tensor) -> float | list[float]:
+    N = len(state1.shape) - 1
+    state1_T = torch.transpose(state1, N, 0)
+    overlap = torch.tensordot(state1_T, state2, dims=N)
+    res: list[float] = list(map(float, torch.abs(overlap**2).flatten()))
+    if len(res) == 1:
+        return res[0]
+    else:
+        return res
+
+
+def Hamiltonian(batch_size: int = 1) -> torch.Tensor:
     sigmaz = torch.diag(torch.tensor([1.0, -1.0], dtype=torch.cdouble))
     Hbase = torch.kron(sigmaz, sigmaz)
     H = torch.kron(Hbase, Hbase)
-    print(H.shape)
+    if batch_size == 1:
+        return H
+    else:
+        return torch.stack((H, H.conj()), dim=2)
 
-    # H = H.unsqueeze(2)
-    print(H.shape)
 
+@pytest.mark.parametrize(
+    "ham_evo",
+    [pyq.HamEvo, pyq.HamEvoEig],
+)
+def test_hamevo_modules_single(ham_evo: torch.nn.Module) -> None:
+    n_qubits = 4
+    H = Hamiltonian(1)
     t_evo = torch.tensor([torch.pi / 4], dtype=torch.cdouble)
-    # t_evo = t_evo.unsqueeze(1)
-    hamevo = pyq.HamEvo(H, t_evo, range(n_qubits), n_qubits)
+    hamevo = ham_evo(H, t_evo, range(n_qubits), n_qubits)
     psi = pyq.uniform_state(n_qubits)
-
     psi_0 = copy.deepcopy(psi)
-
-    def overlap(state1: torch.Tensor, state2: torch.Tensor) -> float:
-        N = len(state1.shape) - 1
-        state1_T = torch.transpose(state1, N, 0)
-        overlap = torch.tensordot(state1_T, state2, dims=N)
-        return float(torch.abs(overlap**2).flatten())
-
     psi_star = hamevo.forward(psi)
-    # result = overlap(psi, psi_star)
-    result: float = overlap(psi_star, psi_0)
-
+    result = overlap(psi_star, psi_0)
+    result = result if isinstance(result, float) else result[0]
     assert isclose(result, 0.5)
+
+
+@pytest.mark.parametrize(
+    "ham_evo",
+    [pyq.HamEvo, pyq.HamEvoEig],
+)
+def test_hamevo_modules_batch(ham_evo: torch.nn.Module) -> None:
+    n_qubits = 4
+    batch_size = 2
+    H = Hamiltonian(batch_size)
+    t_evo = torch.tensor([torch.pi / 4], dtype=torch.cdouble)
+
+    hamevo = ham_evo(H, t_evo, range(n_qubits), n_qubits)
+    psi = pyq.uniform_state(n_qubits, batch_size)
+    psi_0 = copy.deepcopy(psi)
+    psi_star = hamevo.forward(psi)
+    result = overlap(psi_star, psi_0)
+    print(result)
+
+    assert map(isclose, zip(result, [0.5, 0.5]))  # type: ignore [arg-type]
