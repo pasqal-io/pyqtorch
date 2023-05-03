763121c843700623042129e8f6ebb7696683c8bb
Niklas Heim <heim.niklas@gmail.com>
2023-05-03 13:56:39 +0200

    Rework module API (#41)

--- pyqtorch/modules/__init__.py
+++ pyqtorch/modules/__init__.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
-from pyqtorch.modules.circuit import QuantumCircuit, uniform_state, zero_state
+from pyqtorch.modules.circuit import (
+    EntanglingLayer,
+    FeaturemapLayer,
+    QuantumCircuit,
+    VariationalLayer,
+    uniform_state,
+    zero_state,
+)
 from pyqtorch.modules.hamevo import HamEvo, HamEvoEig
 from pyqtorch.modules.parametric import CPHASE, CRX, CRY, CRZ, RX, RY, RZ, U
 from pyqtorch.modules.primitive import CNOT, SWAP, H, I, S, T, X, Y, Z
