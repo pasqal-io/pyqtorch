b8d5be69879dc91cf5c663c6aa60c4422f9023a6
Roland-djee <9250798+Roland-djee@users.noreply.github.com>
2023-07-05 13:44:45 +0100

    Add sdagger op (#85)
    Need to support Sdagger in PyQTorch.

--- pyqtorch/core/operation.py
+++ pyqtorch/core/operation.py
@@ -409,6 +409,26 @@ def S(state: torch.Tensor, qubits: ArrayLike, N_qubits: int) -> torch.Tensor:
     return _apply_gate(state, mat, qubits, N_qubits)
 
 
+def Sdagger(state: torch.Tensor, qubits: ArrayLike, N_qubits: int) -> torch.Tensor:
+    """Sdagger single-qubit gate
+
+    Args:
+        state (torch.Tensor): the input quantum state, of shape `(N_0, N_1,..., N_N, batch_size)`
+        qubits (ArrayLike): list of qubit indices where the gate will operate
+        N_qubits (int): the number of qubits in the system
+
+    Returns:
+        torch.Tensor: the resulting state after applying the gate
+    """
+
+    if ops_cache.enabled:
+        store_operation("Sdagger", qubits)
+
+    dev = state.device
+    mat = OPERATIONS_DICT["SDAGGER"].to(dev)
+    return _apply_gate(state, mat, qubits, N_qubits)
+
+
 def T(state: torch.Tensor, qubits: ArrayLike, N_qubits: int) -> torch.Tensor:
     """T single-qubit gate
 
