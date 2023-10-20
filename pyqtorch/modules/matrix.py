84fab20dc5663bc8ed7b0af26146ccaafeb4d7f4
Roland Guichard <roland.guichard@pasqal.com>
2023-10-20 11:35:53 +0100

    Prototypr for compiled tensordot.

--- pyqtorch/modules/matrix.py
+++ pyqtorch/modules/matrix.py
@@ -1,36 +1,68 @@
+from __future__ import annotations
+
 import torch
 from torch import Tensor
+
 
 class CustomMatrix(Tensor):
     """Prototype for a custom 2x2 matmul."""
 
     def __matmul__(self, other: object):
-        R11 = torch.matmul(self.real[0,:], other.real[:,0]) - torch.matmul(self.imag[0,:], other.imag[:,0])
-        R12 = torch.matmul(self.real[0,:], other.real[:,1]) - torch.matmul(self.imag[0,:], other.imag[:,1])
-        R21 = torch.matmul(self.real[1,:], other.real[:,0]) - torch.matmul(self.imag[1,:], other.imag[:,0]) 
-        R22 = torch.matmul(self.real[1,:], other.real[:,1]) - torch.matmul(self.imag[1,:], other.imag[:,1])
-        I11 = torch.matmul(self.real[0,:], other.imag[:,0]) + torch.matmul(self.imag[0,:], other.real[:,0])
-        I12 = torch.matmul(self.real[0,:], other.imag[:,1]) + torch.matmul(self.imag[0,:], other.real[:,1])
-        I21 = torch.matmul(self.real[1,:], other.imag[:,0]) + torch.matmul(self.imag[1,:], other.real[:,0])
-        I22 = torch.matmul(self.real[1,:], other.imag[:,1]) + torch.matmul(self.imag[1,:], other.real[:,1])
+        R11 = torch.matmul(self.real[0, :], other.real[:, 0]) - torch.matmul(
+            self.imag[0, :], other.imag[:, 0]
+        )
+        R12 = torch.matmul(self.real[0, :], other.real[:, 1]) - torch.matmul(
+            self.imag[0, :], other.imag[:, 1]
+        )
+        R21 = torch.matmul(self.real[1, :], other.real[:, 0]) - torch.matmul(
+            self.imag[1, :], other.imag[:, 0]
+        )
+        R22 = torch.matmul(self.real[1, :], other.real[:, 1]) - torch.matmul(
+            self.imag[1, :], other.imag[:, 1]
+        )
+        I11 = torch.matmul(self.real[0, :], other.imag[:, 0]) + torch.matmul(
+            self.imag[0, :], other.real[:, 0]
+        )
+        I12 = torch.matmul(self.real[0, :], other.imag[:, 1]) + torch.matmul(
+            self.imag[0, :], other.real[:, 1]
+        )
+        I21 = torch.matmul(self.real[1, :], other.imag[:, 0]) + torch.matmul(
+            self.imag[1, :], other.real[:, 0]
+        )
+        I22 = torch.matmul(self.real[1, :], other.imag[:, 1]) + torch.matmul(
+            self.imag[1, :], other.real[:, 1]
+        )
         real_part = torch.tensor([[R11, R12], [R21, R22]])
         imag_part = torch.tensor([[I11, I12], [I21, I22]])
         return torch.complex(real_part, imag_part)
 
+
 @torch.compile
 def custom_matmul(Areal: Tensor, Aimag: Tensor, Breal: Tensor, Bimag: Tensor) -> list:
-    R11 = torch.matmul(Areal[0,:], Breal[:,0]) - torch.matmul(Aimag[0,:], Bimag[:,0])
-    R12 = torch.matmul(Areal[0,:], Breal[:,1]) - torch.matmul(Aimag[0,:], Bimag[:,1])
-    R21 = torch.matmul(Areal[1,:], Breal[:,0]) - torch.matmul(Aimag[1,:], Bimag[:,0]) 
-    R22 = torch.matmul(Areal[1,:], Breal[:,1]) - torch.matmul(Aimag[1,:], Bimag[:,1])
-    I11 = torch.matmul(Areal[0,:], Bimag[:,0]) + torch.matmul(Aimag[0,:], Breal[:,0])
-    I12 = torch.matmul(Areal[0,:], Bimag[:,1]) + torch.matmul(Aimag[0,:], Breal[:,1])
-    I21 = torch.matmul(Areal[1,:], Bimag[:,0]) + torch.matmul(Aimag[1,:], Breal[:,0])
-    I22 = torch.matmul(Areal[1,:], Bimag[:,1]) + torch.matmul(Aimag[1,:], Breal[:,1])
+    R11 = torch.matmul(Areal[0, :], Breal[:, 0]) - torch.matmul(Aimag[0, :], Bimag[:, 0])
+    R12 = torch.matmul(Areal[0, :], Breal[:, 1]) - torch.matmul(Aimag[0, :], Bimag[:, 1])
+    R21 = torch.matmul(Areal[1, :], Breal[:, 0]) - torch.matmul(Aimag[1, :], Bimag[:, 0])
+    R22 = torch.matmul(Areal[1, :], Breal[:, 1]) - torch.matmul(Aimag[1, :], Bimag[:, 1])
+    I11 = torch.matmul(Areal[0, :], Bimag[:, 0]) + torch.matmul(Aimag[0, :], Breal[:, 0])
+    I12 = torch.matmul(Areal[0, :], Bimag[:, 1]) + torch.matmul(Aimag[0, :], Breal[:, 1])
+    I21 = torch.matmul(Areal[1, :], Bimag[:, 0]) + torch.matmul(Aimag[1, :], Breal[:, 0])
+    I22 = torch.matmul(Areal[1, :], Bimag[:, 1]) + torch.matmul(Aimag[1, :], Breal[:, 1])
     real_part = torch.tensor([[R11, R12], [R21, R22]])
     imag_part = torch.tensor([[I11, I12], [I21, I22]])
     return real_part, imag_part
-    
+
+
+@torch.compile
+def custom_tensordot(Areal: Tensor, Aimag: Tensor, Breal: Tensor, Bimag: Tensor) -> list:
+    R11 = torch.matmul(Areal[0, :], Breal) - torch.matmul(Aimag[0, :], Bimag)
+    R21 = torch.matmul(Areal[1, :], Breal) - torch.matmul(Aimag[1, :], Bimag)
+    I11 = torch.matmul(Areal[0, :], Bimag) + torch.matmul(Aimag[0, :], Breal)
+    I21 = torch.matmul(Areal[1, :], Bimag) + torch.matmul(Aimag[1, :], Breal)
+    real_part = torch.tensor([[R11], [R21]])
+    imag_part = torch.tensor([[I11], [I21]])
+    return real_part, imag_part
+
+
 # @torch.compile
 def foo(x, y):
     a = torch.sin(x)
--- pyqtorch/modules/test.py
+++ pyqtorch/modules/test.py
@@ -1,23 +1,50 @@
-from matrix import CustomMatrix, custom_matmul, foo
+from __future__ import annotations
+
 import torch
-import torch._dynamo
-torch._dynamo.config.suppress_errors = True
+from matrix import custom_tensordot
 
-A = torch.rand(2,2, dtype=torch.complex128)
-B = torch.rand(2,2, dtype=torch.complex128)
+# import torch._dynamo
+# torch._dynamo.config.suppress_errors = True
 
-C = torch.matmul(A,B)
-print(C)
+A = torch.rand(2, 2, dtype=torch.complex128)
+B = torch.rand(2, dtype=torch.complex128)
 
-AA = CustomMatrix(A)
-BB = CustomMatrix(B)
+# for i in [10**n for n in range(2,7)]:
+#     st = time.time()
+#     for _ in range(i):
+#         C = torch.matmul(A,B)
+#     et = time.time()
+#     print(f"torch.matmul {i} took {et - st}")
 
-CC = AA @ BB
-print(CC)
+#     AA = CustomMatrix(A)
+#     BB = CustomMatrix(B)
 
-real_part, imag_part = custom_matmul(A.real, A.imag, B.real, B.imag)
-print(torch.complex(real_part, imag_part))
+#     st = time.time()
+#     for _ in range(i):
+#         CC = AA @ BB
+#     et = time.time()
+#     print(f"CustomMatrix.matmul {i} took {et - st}")
+
+#     # opt_matmul = torch.compile(custom_matmul)
+#     # real_part, imag_part = opt_matmul(A.real, A.imag, B.real, B.imag)
+#     # print(torch.complex(real_part, imag_part))
+
+#     st = time.time()
+#     for _ in range(i):
+#         real_part, imag_part = custom_matmul(A.real, A.imag, B.real, B.imag)
+#     et = time.time()
+
+#     res = torch.complex(real_part, imag_part)
+#     print(f"torch.compile {i} took {et - st}\n")
 
 # opt_foo = torch.compile(foo)
 
 # print(opt_foo(torch.randn(10, 10), torch.randn(10, 10)))
+
+C = torch.matmul(A, B)
+CC = torch.tensordot(A, B, dims=1)
+print(f"C {C} CC {CC}")
+
+real_part, imag_part = custom_tensordot(A.real, A.imag, B.real, B.imag)
+final_tensor = torch.complex(real_part, imag_part).squeeze()
+print(f"CCC {final_tensor}")
