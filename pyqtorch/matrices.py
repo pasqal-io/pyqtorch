[Fix] Matrix precision and use no_grad in adjoint (#126)
Added missing dtype to matrix inits which causes issues in qadence.
added no_grad decorator to first order adjoint, bump version

Co-authored-by: Joao P. Moutinho <joao.moutinho@pasqal.com>

pyqtorch/matrices.py
@@ -11,7 +11,8 @@ ZMAT = torch.tensor([[1, 0], [0, -1]], dtype=DEFAULT_MATRIX_DTYPE)
 SMAT = torch.tensor([[1, 0], [0, 1j]], dtype=DEFAULT_MATRIX_DTYPE)
 SDAGGERMAT = torch.tensor([[1, 0], [0, -1j]], dtype=DEFAULT_MATRIX_DTYPE)
 TMAT = torch.tensor(
-    [[1, 0], [0, torch.exp(torch.tensor(1.0j * torch.pi / 4))]], dtype=DEFAULT_MATRIX_DTYPE
+    [[1, 0], [0, torch.exp(torch.tensor(1.0j * torch.pi / 4, dtype=DEFAULT_MATRIX_DTYPE))]],
+    dtype=DEFAULT_MATRIX_DTYPE,
 )
 NMAT = torch.tensor([[0, 0], [0, 1]], dtype=DEFAULT_MATRIX_DTYPE)
 NDIAG = torch.tensor([0, 1], dtype=DEFAULT_MATRIX_DTYPE)
@@ -33,7 +34,11 @@ CSWAPMAT = torch.tensor(
     ],
     dtype=DEFAULT_MATRIX_DTYPE,
 )
-HMAT = 1 / torch.sqrt(torch.tensor(2)) * torch.tensor([[1, 1], [1, -1]], dtype=DEFAULT_MATRIX_DTYPE)
+HMAT = (
+    1
+    / torch.sqrt(torch.tensor(2.0, dtype=DEFAULT_MATRIX_DTYPE))
+    * torch.tensor([[1, 1], [1, -1]], dtype=DEFAULT_MATRIX_DTYPE)
+)
 
 
 OPERATIONS_DICT = {