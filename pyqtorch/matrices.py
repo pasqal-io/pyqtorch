8f76cf44a64896fa1e41c17cc729d2007d049c75
Roland Guichard <roland.guichard@pasqal.com>
2023-11-23 16:16:56 +0000

    Correct projector matrix generation.

--- pyqtorch/matrices.py
+++ pyqtorch/matrices.py
@@ -43,7 +43,9 @@ HMAT = (
 
 
 def PROJMAT(ket: Tensor, bra: Tensor) -> Tensor:
-    return torch.kron(ket, bra.T)
+    # Reshape to conform internal PyQ definition for states and operators.
+    proj_mat = torch.kron(ket, bra.T).reshape((2,) * ket.size(0))
+    return proj_mat.unsqueeze(-1)
 
 
 OPERATIONS_DICT = {
