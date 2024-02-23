5df42a521b025c4527dda50793a176af372c2581
seitzdom <dominik.seitz@pasqal.com>
2024-02-23 18:17:40 +0100

    modelparallelcircuit constructor

--- pyqtorch/circuit.py
+++ pyqtorch/circuit.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from logging import getLogger
 from typing import Any, Iterator
 
+import torch
 from torch import Tensor
 from torch import device as torch_device
 from torch.nn import Module, ModuleList
@@ -106,3 +107,80 @@ def expectation(
         return AdjointExpectation.apply(circuit, observable, state, values.keys(), *values.values())
     else:
         raise ValueError(f"Requested diff_mode '{diff_mode}' not supported.")
+
+
+class PipedCircuit(QuantumCircuit):
+    def __init__(self, n_qubits: int, operations: list[Module], dev_idx: int):
+        super().__init__(n_qubits, operations)
+        self = self.to(torch_device(f"cuda:{dev_idx}"))
+
+    def run(self, state: State = None, values: dict[str, Tensor] = {}) -> State:
+        if state is None:
+            state = self.init_state()
+        else:
+            state = state.to(self.device)
+        values = {k: v.to(self.device) for k, v in values.items()}
+        for op in self.operations:
+            state = op(state, values)
+        return state
+
+
+class ModelParallelCircuit(QuantumCircuit):
+    def __init__(self, circ: QuantumCircuit, n_devices: int):
+        if not all([isinstance(subc, QuantumCircuit) for subc in circ.operations]):
+            msg = "Make sure the passed QuantumCircuit only contains other QuantumCircuits."
+            logger.error(msg)
+            raise ValueError(msg)
+        if not torch.cuda.is_available():
+            msg = f"{self.__class__.__name__} requires at least two GPU devices."
+            logger.error(msg)
+            raise ValueError(msg)
+        dev_count = torch.cuda.device_count()
+        if dev_count < n_devices:
+            msg = f"Requested {n_devices} GPU devices however only {dev_count} devices available."
+            logger.error(msg)
+            raise ValueError(msg)
+        n_circs = len(circ.operations)
+        dev_indices = [i for i in range(n_devices) for _ in range(n_circs // n_devices)]
+        operations = [
+            PipedCircuit(c.n_qubits, c.operations, dev_idx)
+            for c, dev_idx in zip(circ.operations, dev_indices)
+        ]
+        super().__init__(circ.n_qubits, operations)
+
+
+# class PipelineParallelCircuit(ModelParallelCircuit):
+#     def __init__(self, split_size=N_POINTS // 2, *args, **kwargs):
+#         super(PipelineParallelCircuit, self).__init__(*args, **kwargs)
+#         self.split_size = split_size
+
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         init_state = zero_state(N_QUBITS).to("cuda:0")
+#         splits = iter(x.split(self.split_size, dim=0))
+#         s_next = next(splits)
+#         s_prev = self.feature_map(init_state, {"x": s_next.to("cuda:0")})
+#         s_prev = self.c0.forward(s_prev, self.params_c0).to("cuda:1")
+#         ret = []
+
+#         for s_next in splits:
+#             s_prev = self.c1.forward(s_prev, self.params_c1)
+#             ret.append(inner_prod(s_prev, self.observable.forward(s_prev)).real)
+
+#             s_prev = self.feature_map(init_state, {"x": s_next.to("cuda:0")})
+#             s_prev = self.c0.forward(s_prev, self.params_c0).to("cuda:1")
+
+#         s_prev = self.c1.forward(s_prev, self.params_c1)
+#         ret.append(inner_prod(s_prev, self.observable.forward(s_prev)).real)
+#         return torch.cat(ret)
+
+
+# def train(circ) -> None:
+#     optimizer = torch.optim.Adam(
+#         {**circ.params_c0, **circ.params_c1}.values(), lr=0.01, foreach=False
+#     )
+#     for epoch in range(N_EPOCHS):
+#         optimizer.zero_grad()
+#         y_pred = circ.forward(x)
+#         loss = mse_loss(y_pred, y.to("cuda:1"))
+#         loss.backward()
+#         optimizer.step()
