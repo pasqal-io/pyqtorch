e27546899d95a702b38b1d12c9ebfe33a376a0a8
EthanObadia <160495948+EthanObadia@users.noreply.github.com>
2024-05-31 17:37:14 +0200

    Update tests/test_circuit.py
    Co-authored-by: João P. Moutinho <joao.moutinho@pasqal.com>

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -64,7 +64,7 @@ def test_differentiate_circuit(dtype: torch.dtype, batch_size: int, n_qubits: in
         pyq.Y(1),
         pyq.RX(0, "theta_0"),
         pyq.PHASE(0, "theta_1"),
-        pyq.CSWAP(2, (0, 1)),
+        pyq.CSWAP(0, (1, 2)),
         pyq.CRX(1, 2, "theta_2"),
         pyq.CPHASE(1, 2, "theta_3"),
         pyq.CNOT(0, 1),
