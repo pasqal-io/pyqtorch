3348819d6af2e7c5ce64d52a3cb5f53363928af4
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2024-08-06 13:11:10 +0200

    noise

---
+++ pyqtorch/noise/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from .gates import (
+    AmplitudeDamping,
+    BitFlip,
+    Depolarizing,
+    GeneralizedAmplitudeDamping,
+    Noise,
+    NoiseProtocol,
+    PauliChannel,
+    PhaseDamping,
+    PhaseFlip,
+    _repr_noise,
+)
