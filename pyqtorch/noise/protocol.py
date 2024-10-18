4282662377e51c28869be7fb27c520fc0329480b
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-10-18 11:27:25 +0200

    rm readout from noise protocol

--- pyqtorch/noise/protocol.py
+++ pyqtorch/noise/protocol.py
@@ -14,7 +14,6 @@ class NoiseType(StrEnum):
     AMPLITUDE_DAMPING = "AmplitudeDamping"
     PHASE_DAMPING = "PhaseDamping"
     GENERALIZED_AMPLITUDE_DAMPING = "GeneralizedAmplitudeDamping"
-    READOUT = "Readout"
 
 
 @dataclass
@@ -152,10 +151,6 @@ class NoiseProtocol:
     @classmethod
     def generalized_amplitude_damping(cls, *args, **kwargs) -> NoiseProtocol:
         return cls(NoiseType.GENERALIZED_AMPLITUDE_DAMPING, *args, **kwargs)
-
-    @classmethod
-    def readout(cls, *args, **kwargs) -> NoiseProtocol:
-        return cls(NoiseType.READOUT, *args, **kwargs)
 
     def __repr__(self) -> str:
         if self.len == 1:
